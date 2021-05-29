e1fcd46c2dfc734fd095bffc9825a8510c66e2f7
SanpuiRonak <ronaksanpui@gmail.com>
2021-05-29 21:22:51 +0530

    graphics anlam

--- src/components/cell.jsx
+++ src/components/cell.jsx
@@ -1,24 +1,39 @@
 import "../style.css";
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import cross from "../resources/cross.png";
+import circle from "../resources/circle.png";
 
 class Cell extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      value:null,
-    }
+      value: null,
+    };
   }
-  state = {  }
-  
-  render() { 
-    
-    return (<div className="cell" onClick={()=>{ this.props.onClick() }}>{this.props.value}</div>  );
-  }
+  state = {};
 
-  
+  render() {
+    let imgSrc;
+
+    if (this.props.value === "X") {
+      imgSrc = cross;
+    } else if (this.props.value === "O") {
+      imgSrc = circle;
+    } else {
+      imgSrc = "";
+    }
+
+    return (
+      <div
+        className="cell"
+        onClick={() => {
+          this.props.onClick();
+        }}
+      >
+        <img src={imgSrc} />
+      </div>
+    );
+  }
 }
- 
 
 export default Cell;
