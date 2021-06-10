a8d2484591d481822ecc3ad5729eaef0cdba3398
SanpuiRonak <ronaksanpui@gmail.com>
2021-06-10 22:07:22 +0530

    mobile ok

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import Table from "./components/table";
 ReactDOM.render(
   <React.StrictMode>
     <Table />
+    <button
+      className="reloadButton"
+      onClick={() => {
+        window.location.reload();
+      }}
+    >
+      Play Again
+    </button>
   </React.StrictMode>,
   document.getElementById("root")
 );
